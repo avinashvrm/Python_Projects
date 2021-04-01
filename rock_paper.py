27e1d93547aeda6c00428fb920db6f261d596f7a
avinashvrm <66836647+avinashvrm@users.noreply.github.com>
2021-04-01 18:47:21 +0530

    Update rock_paper.py

--- rock_paper.py
+++ rock_paper.py
@@ -1,12 +1,13 @@
 import random
 choices = ["Rock", "Paper", "Scissors"]
-computer = random.choice(choices)
+
 player = False
 cpu_score = 0
 player_score = 0
 while True:
     player = input("Rock, Paper or  Scissors?").capitalize()
     ## Conditions of Rock,Paper and Scissors
+    computer = random.choice(choices)
     if player == computer:
         print("Tie!")
     elif player == "Rock":
